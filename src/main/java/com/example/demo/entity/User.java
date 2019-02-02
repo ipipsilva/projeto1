9c837f3cf7ebb4418d0e2d12d6b1a5a349985541
Igor Silva <psilva.igor@gmail.com>
2019-02-02 18:45:41 -0200

    primeiro comite

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,55 @@
+package com.example.demo.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String email;
+
+	@ManyToMany
+	private List<Role> perfis;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Role> getPerfis() {
+		return perfis;
+	}
+
+	public void setPerfis(List<Role> perfis) {
+		this.perfis = perfis;
+	}
+}
